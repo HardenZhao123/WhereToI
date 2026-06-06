8f125c53b808a479b535d2331f798a84aab0a1a3
Zihao Zhao <zz6724@ic.ac.uk>
2026-06-06 21:21:16 +0100

    fix: add userId variable to recordAccess function parameters in postgres-repository.mjs

--- src/app/controllers/map-controller.js
+++ src/app/controllers/map-controller.js
@@ -1262,6 +1262,7 @@ export function createMapController(elements, onToiletSelected = () => {}, auth 
 
     if (isAuthenticated()) {
       recordAccessHistory({
+        toiletId: selectedToilet.id,
         toiletName: selectedToilet.name,
         eventType: "Directions",
         amountGbp: 0,
